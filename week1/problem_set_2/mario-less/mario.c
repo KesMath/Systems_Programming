bf264a986984bd68ec13e953205e17595f529893
Kesler Mathieu <kesmathieu30@gmail.com>
2022-06-25 22:02:30 -0400

    fleshing out template

--- week1/problem_set_2/mario-less/mario.c
+++ week1/problem_set_2/mario-less/mario.c
@@ -1,7 +1,23 @@
 #include <cs50.h>
 #include <stdio.h>
 
+void print_right_aligned_pyramid_ascii_art(char, int);
+
 int main(void) 
 {
-    
+    int height;
+    do{
+        int height = get_int("Height: ");
+    }
+    while(height < 1 && height > 8);
+    print_right_aligned_pyramid_ascii_art('#', height);
+}
+
+void print_right_aligned_pyramid_ascii_art(char ch, int height){
+   // prints char in right-aligned pyramid
+   // for example: height = 4 yields:
+   //   #
+   //  ##
+   // ###
+   //####
 }
