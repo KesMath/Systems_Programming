4e871b39b28ef7c85462ea73d08863869d310bcd
Kesler Mathieu <kesmathieu30@gmail.com>
2022-06-25 23:16:15 -0400

    updating compiler flags to mirror cs50 and adjusting code logic

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gcc -lcs50 $1.c -o $1 && ./$1
+clang -lcs50 -lm -fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 -std=c11 -Wall -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter -Wno-unused-variable -Wshadow $1.c -o $1 && ./$1
--- week1/problem_set_2/mario-less/mario.c
+++ week1/problem_set_2/mario-less/mario.c
@@ -7,17 +7,23 @@ int main(void)
 {
     int height;
     do{
-        int height = get_int("Height: ");
+        height = get_int("Height: ");
     }
-    while(height < 1 && height > 8);
+    while(height < 1 || height > 8);
     print_right_aligned_pyramid_ascii_art('#', height);
 }
 
 void print_right_aligned_pyramid_ascii_art(char ch, int height){
-   // prints char in right-aligned pyramid
+   // prints char in a right-aligned pyramid formation
    // for example: height = 4 yields:
    //   #
    //  ##
    // ###
    //####
+    for(int i = 1; i <= height; i++){
+        for(int j = 0; j < i; j++){
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
 }
