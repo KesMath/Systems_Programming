f64ae123f835cdbc0d345a8b151e8fca58723c24
Kesler Mathieu <kesmathieu30@gmail.com>
2022-07-03 21:25:17 -0400

    finalizing function

--- print_to_stdout.py
+++ print_to_stdout.py
@@ -1,3 +1,9 @@
+# python3 print_to_stdout.py
+
+import sys
+assert sys.version_info >= (3, 5), \
+    "Python Version " + str(sys.version_info[0]) + "." + str(sys.version_info[1]) + " is not compatible with this application!\nInterpreter Version must be >= 3.5"
+import math
 
 def lcm(a: int, b: int):
     '''
@@ -15,41 +21,49 @@ def lcm(a: int, b: int):
     elif(isinstance(a, float) or isinstance(b, float)):
         raise TypeError("LCM() cannot be conducted on floats!")
     else:
-        return abs(a) * abs(b)
+        return abs(a*b) / math.gcd(a,b)
 
-def print_to_stdout(iterations: int, string1: str, string2: str):
+def print_to_stdout(iterations: int, int1: int, str1: str, int2: int, str2: str):
     '''
         Conditionally prints numbers 1 -> N to Standard Output.
         The condition is as follows:
-            whe
+            - when n divisible by int1 & int2, print concat(str1, str2)
+            - when n divisible by int1, print str1
+            - when n divisible by int2, print str2
+        Raises Typerror when either int1 or int2 are floating points!
+        (This function is scoped solely to use integers and as such, 
+        do not want to deal with the subject of modulo operator and irrational numbers)
 
                 Parameters:
                         iterations (int):  upper bound of cycles to print to std. out
-                        string1    (str):  string to be printed when N divisible by 3
-                        string2    (str):  string to be printed when N divisible by 5
+                        str1       (str):  string to be printed when N divisible by int1
+                        str2       (str):  string to be printed when N divisible by int2
                 Returns:
                         N/A - void method
         '''
-    if (string1 is not None) and (string2 is not None) and (iterations > 0):
-        #for a number to be divisible by A & B is equivalent to least common multiple or LCM of(A*B)
-        lcm = lcm(3, 5)
-        for i in range(1, iterations + 1):
+    if(isinstance(int1, float) or isinstance(int2, float)):
+        raise TypeError("Function will not perform modulo using floating points")
 
-            if i % lcm == 0:
-               print(string1 + string2)
+    else:
+        if (str1 is not None) and (str2 is not None) and (iterations > 0) and (int1 > 0) and (int2 > 0):
+            #for a number to be divisible by A & B is equivalent to least common multiple or LCM(A,B)
+            least_multiple = lcm(int1, int2)
+            for i in range(1, iterations + 1):
 
-            elif i % 3 == 0:
-                print(string1)
+                if i % least_multiple == 0:
+                    print(str1 + str2)
 
-            elif i % 5 == 0:
-                print(string2)
+                elif i % int1 == 0:
+                    print(str1)
 
-            else:
-                print(i) 
+                elif i % int2 == 0:
+                    print(str2)
+
+                else:
+                    print(i) 
 
 def main():
-    #print(lcm(3, 5))
-    #print_to_stdout(100, "Crackle", "Pop")
+    print_to_stdout(100, 3, "Crackle", 5, "Pop")
 
 if __name__ == '__main__':
     main()
