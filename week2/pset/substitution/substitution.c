a71f82ef14f4f0c64d281eb89933db8aa5095549
Kesler Mathieu <kesmathieu30@gmail.com>
2022-07-14 18:33:19 -0400

    submitting template code

---
+++ week2/pset/substitution/substitution.c
@@ -0,0 +1,17 @@
+#include <cs50.h>
+#include <stdio.h>
+
+string get_substitution_cipher(string plaintext, string substitution_key);
+
+int main(int argc, string argv[]) 
+{
+
+}
+
+// IMPLEMENTATION:
+// for every char in plaintext, downsize to fit between 0 <= x <= 25
+// then let cipher[i] = substitution_key[plaintext[i]];
+// finally, return cipher!
+string get_substitution_cipher(string plaintext, string substitution_key){
+
+}
