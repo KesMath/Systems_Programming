85c81549a01c4d834b2f47901902fa439f50c11f
Kesler Mathieu <kesmathieu30@gmail.com>
2022-07-14 16:07:12 -0400

    adding commentary and renaming constants

--- week2/pset/caesar/caesar.c
+++ week2/pset/caesar/caesar.c
@@ -4,47 +4,54 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-const int ASCII_UPPER_CASE_LOWER_BOUND = 64;
-const int ASCII_UPPER_CASE_UPPER_BOUND = 90;
+const int ASCII_UPPER_CASE_STARTING_BOUNDARY = 64;
+const int ASCII_UPPER_CASE_ENDING_BOUNDARY = 90;
 
-const int ASCII_LOWER_CASE_LOWER_BOUND = 96;
-const int ASCII_LOWER_CASE_UPPER_BOUND = 122;
+const int ASCII_LOWER_CASE_STARTING_BOUNDARY = 96;
+const int ASCII_LOWER_CASE_ENDING_BOUNDARY = 122;
 
 string get_caesar_cipher(string plaintext, int shift_key);
-bool is_integer_str(string text);
+bool is_only_digits(string text);
 
 int main(int argc, string argv[]) 
 {
-    if (argc == 2){
+    if (argc == 2 && is_only_digits(argv[1])){
         string plaintext = get_string("plaintext: ");
-        if(is_integer_str(argv[1])){
-            printf("ciphertext: %s\n", get_caesar_cipher(plaintext, atoi(argv[1])));
-        }
+        printf("ciphertext: %s\n", get_caesar_cipher(plaintext, atoi(argv[1])));
+        return 0;
     }
     else{
         printf("Usage: ./caesar key\n");
     }
+    return 1;
 }
 
+// Similar approach to their rotate() function
+// but instead of downsizing upper case and lower case to fit into
+// the bounds of [0,25], this approach retains the original ASCII bounds
+// 65 <= uppercase <= 90, 97 <= lowercase <= 122, performs the shift,
+// and uses respective modulo (uppercase bound = 64, lowercase = 96)
+// to generate new uppercase or lowercase cipher
 string get_caesar_cipher(string plaintext, int shift_key){
     int str_len = strlen(plaintext);
     for(int i = 0; i < str_len; i++){
-        //in-place approach without auxillary arrays which saves 26 bytes * 8 bits = 206 bits of memory!
+        // in-place approach
         if(isalpha(plaintext[i])){
             if(isupper(plaintext[i])){
-                if(plaintext[i] + shift_key > ASCII_UPPER_CASE_UPPER_BOUND){
-                    plaintext[i] = ((plaintext[i] + shift_key) % ASCII_UPPER_CASE_UPPER_BOUND) + ASCII_UPPER_CASE_LOWER_BOUND;
+                if(plaintext[i] + shift_key > ASCII_UPPER_CASE_ENDING_BOUNDARY){
+                    // adding the ASCII_UPPER_CASE_STARTING_BOUNDARY assures char wraps around into uppercase ascii territory
+                    plaintext[i] = ((plaintext[i] + shift_key) % ASCII_UPPER_CASE_ENDING_BOUNDARY) + ASCII_UPPER_CASE_STARTING_BOUNDARY;
                 }
                 else{
-                    plaintext[i] = (plaintext[i] + shift_key) % ASCII_UPPER_CASE_UPPER_BOUND;
+                    plaintext[i] = (plaintext[i] + shift_key) % ASCII_UPPER_CASE_ENDING_BOUNDARY;
                 }
             }
             else{
-                if(plaintext[i] + shift_key > ASCII_LOWER_CASE_UPPER_BOUND){
-                    plaintext[i] = ((plaintext[i] + shift_key) % ASCII_LOWER_CASE_UPPER_BOUND) + ASCII_LOWER_CASE_LOWER_BOUND;
+                if(plaintext[i] + shift_key > ASCII_LOWER_CASE_ENDING_BOUNDARY){
+                    plaintext[i] = ((plaintext[i] + shift_key) % ASCII_LOWER_CASE_ENDING_BOUNDARY) + ASCII_LOWER_CASE_STARTING_BOUNDARY;
                 }
                 else{
-                    plaintext[i] = (plaintext[i] + shift_key) % ASCII_LOWER_CASE_UPPER_BOUND;
+                    plaintext[i] = (plaintext[i] + shift_key) % ASCII_LOWER_CASE_ENDING_BOUNDARY;
                 }
             }
         }
@@ -54,7 +61,7 @@ string get_caesar_cipher(string plaintext, int shift_key){
     return plaintext;
 }
 
-bool is_integer_str(string text){
+bool is_only_digits(string text){
     bool is_integer_str = true;
     int len_str = strlen(text);
     for (int i = 0; i < len_str; i++){
