be07dfdd380ec9382c83c4ae8fa458f264ef5a39
Kesler Mathieu <kesmathieu30@gmail.com>
2022-07-16 17:07:31 -0400

    removing redundant return statements

--- week2/pset/substitution/substitution.c
+++ week2/pset/substitution/substitution.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
-bool contains_all_alphas_at_least_once(string text);
+bool contains_repeated_chars(string text);
 string is_only_alphabetical(string text);
 string get_substitution_cipher(string plaintext, string substitution_key);
 
@@ -11,23 +11,21 @@ int main(int argc, string argv[])
     if (argc == 2){
         if(strlen(argv[1]) != 26){
             printf("Key must be 26 characters long!\n");
-            return 1;
         }
         else{
             if(!is_only_alphabetical(argv[1])){
                 printf("Key must be alphabetical letters only!\n");
-                return 1;
             }
             else{
-                if(!contains_all_alphas_at_least_once(argv[1])){
-                    printf("Key must contain all alphabetical characters at least once!\n");
-                    return 1;  
+                if(contains_repeated_chars(argv[1])){
+                    printf("Key must contain all alphabetical characters at least once!\n"); 
                 }
                 string plaintext = get_string("plaintext: ");
                 printf("ciphertext: %s\n", get_substitution_cipher(plaintext, argv[1]));
                 return 0;
             }
         }
+        return 1;
     }
     else{
         printf("./substitution key");
@@ -44,7 +42,7 @@ string get_substitution_cipher(string plaintext, string substitution_key){
 
 }
 
-bool contains_all_alphas_at_least_once(string text){
+bool contains_repeated_chars(string text){
 
 }
 
