f1e1105efe49099e2104fd2d462c31d0b05b5303
Kesler Mathieu <kesmathieu30@gmail.com>
2022-07-26 18:30:15 -0400

    done testing

--- week3/pset/plurality/plurality.c
+++ week3/pset/plurality/plurality.c
@@ -25,56 +25,59 @@ void print_winner(void);
 
 int main(int argc, string argv[])
 {
-    // // Check for invalid usage
-    // if (argc < 2)
-    // {
-    //     printf("Usage: plurality [candidate ...]\n");
-    //     return 1;
-    // }
-
-    // // Populate array of candidates
-    // candidate_count = argc - 1;
-    // if (candidate_count > MAX)
-    // {
-    //     printf("Maximum number of candidates is %i\n", MAX);
-    //     return 2;
-    // }
-    // for (int i = 0; i < candidate_count; i++)
-    // {
-    //     candidates[i].name = argv[i + 1];
-    //     candidates[i].votes = 0;
-    // }
-
-    // int voter_count = get_int("Number of voters: ");
-
-    // // Loop over all voters
-    // for (int i = 0; i < voter_count; i++)
-    // {
-    //     string name = get_string("Vote: ");
-
-    //     // Check for invalid vote
-    //     if (!vote(name))
-    //     {
-    //         printf("Invalid vote.\n");
-    //     }
-    // }
-
-
-    //TESTING!!!
-    int array[] = {2, 1, 3, 7, 4};
-    string arr[] = {"pat", "mat", "cat", "dat", "frat"};
-
-    for (int i = 0; i < 5; i++)
+    // Check for invalid usage
+    if (argc < 2)
     {
-        candidates[i].name = arr[i];
-        candidates[i].votes = array[i];
+        printf("Usage: plurality [candidate ...]\n");
+        return 1;
     }
-    // DONE TESTING!!!
 
+    // Populate array of candidates
+    candidate_count = argc - 1;
+    if (candidate_count > MAX)
+    {
+        printf("Maximum number of candidates is %i\n", MAX);
+        return 2;
+    }
+    for (int i = 0; i < candidate_count; i++)
+    {
+        candidates[i].name = argv[i + 1];
+        candidates[i].votes = 0;
+    }
 
-    // Display winner of election
+    int voter_count = get_int("Number of voters: ");
+
+    // Loop over all voters
+    for (int i = 0; i < voter_count; i++)
+    {
+        string name = get_string("Vote: ");
+
+        // Check for invalid vote
+        if (!vote(name))
+        {
+            printf("Invalid vote.\n");
+        }
+    }
     print_winner();
-}
+
+
+//     //TESTING!!!
+//     int array[] = {2, 1, 3, 7, 1};
+//     string arr[] = {"pat", "mat", "cat", "dat", "frat"};
+
+//     for (int i = 0; i < 5; i++)
+//     {
+//         candidates[i].name = arr[i];
+//         candidates[i].votes = array[i];
+//     }
+
+//     print_winner();
+//     for (int i = 0; i < 5; i++)
+//     {
+//         printf("%i\n", candidates[i].votes);
+//     }
+//     //DONE TESTING!!!
+// }
 
 // Update vote totals given a new vote
 // A Better approach would be to use dictionary which would reduce O(n*m) runtime in main() to O(n)
@@ -98,6 +101,8 @@ void swap(candidate arr[], int index1, int index2){
 
 void selection_sort(void){
     // selection sort candidate array from least to greatest votes
+    //TODO: for Testing Purposes ONLY!
+    //int candidate_count = 5;
     for(int i = 0; i < candidate_count; i++){
         for(int j = i + 1; j < candidate_count; j++){
             if(candidates[i].votes > candidates[j].votes){
