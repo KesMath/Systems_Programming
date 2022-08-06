2620f7e9fbf2aba9ca244359925590f4b66e417c
Kesler Mathieu <kesmathieu30@gmail.com>
2022-08-06 13:11:15 -0400

    submitting working solution

--- week4/lab/volume/volume.c
+++ week4/lab/volume/volume.c
@@ -39,16 +39,16 @@ int main(int argc, char *argv[])
     // what's the benefit of saving it on the heap or stack?
     uint8_t header_buffer[HEADER_SIZE];
 
-    // fread() is gonna move filepointer 8 bits at a time, 44 times and copy results into header_buffer
+    // fread() is gonna move filepointer 8 bits/ 1 byte at a time, 44 times and copy results into header_buffer
     // it returns number of items read ... in this case that's 44 bytes 
-    size_t bytes_read = fread(header_buffer, sizeof(u_int8_t), HEADER_SIZE, input);
+    size_t bytes_read = fread(header_buffer, sizeof(uint8_t), HEADER_SIZE, input);
 
     if(bytes_read != HEADER_SIZE){
         printf("Was not able to read all %i bytes successfully", HEADER_SIZE);
         return 1;
     }
     
-    // fwrite() going to ingest that buffer, 8 bits at a time, and write that to output file, 44 times
+    // fwrite() going to ingest that buffer, 8 bits/ 1 byte at a time, and write that to output file, 44 times
     // it returns number of items written ... in this case that's 44 bytes
     size_t bytes_written = fwrite(header_buffer, sizeof(uint8_t), HEADER_SIZE, output);
 
@@ -58,11 +58,12 @@ int main(int argc, char *argv[])
     }
 
     // TODO: Read samples from input file and write updated data to output file
-    uint16_t *audio_sample = NULL;
-    while(fread(audio_sample, sizeof(u_int16_t), 1, input) != sizeof(uint16_t)){
-        //from right to left, get value at that pointer by dereferencing, multiply by factor
-        *audio_sample = factor * (*audio_sample);
-        fwrite(audio_sample, sizeof(u_int16_t), 1, output);
+    uint16_t audio_sample;
+
+    // reading 16 bits / 2 bytes one at a time until fread() returns 0 bytes read meaning end of file!
+    while(fread(&audio_sample, sizeof(u_int16_t), 1, input) == 1){
+        audio_sample *= factor;
+        fwrite(&audio_sample, sizeof(u_int16_t), 1, output);
     }
 
     // Close files
