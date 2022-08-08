26005108fa6ae8302bb58deea2ec73aae734707f
Kesler Mathieu <kesmathieu30@gmail.com>
2022-08-08 15:10:10 -0400

    changing datatype and rounding effectively

--- week4/pset/filter-less/helpers.c
+++ week4/pset/filter-less/helpers.c
@@ -1,4 +1,5 @@
 #include "helpers.h"
+#include "math.h"
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
@@ -6,10 +7,10 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     for(int i = 0; i < height; i++){
         for(int j = 0; j < width; j++){
             //take average of all 3 colors to get the intensity of gray for that pixel and set all pixels to that average
-            int grayscale_intensity = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed / 3;
-            image[i][j].rgbtBlue = grayscale_intensity;
-            image[i][j].rgbtGreen = grayscale_intensity;
-            image[i][j].rgbtRed = grayscale_intensity;
+            double grayscale_intensity = round(image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed / 3);
+            image[i][j].rgbtBlue = (BYTE) grayscale_intensity;
+            image[i][j].rgbtGreen = (BYTE) grayscale_intensity;
+            image[i][j].rgbtRed = (BYTE) grayscale_intensity;
         }
     }
     return;
