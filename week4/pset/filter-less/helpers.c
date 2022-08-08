670b68c1120701920951c1cdf48d436ebd1dc349
Kesler Mathieu <kesmathieu30@gmail.com>
2022-08-08 14:59:06 -0400

    adding implementation for grayscale

--- week4/pset/filter-less/helpers.c
+++ week4/pset/filter-less/helpers.c
@@ -3,6 +3,15 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+            //take average of all 3 colors to get the intensity of gray for that pixel and set all pixels to that average
+            int grayscale_intensity = image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed / 3;
+            image[i][j].rgbtBlue = grayscale_intensity;
+            image[i][j].rgbtGreen = grayscale_intensity;
+            image[i][j].rgbtRed = grayscale_intensity;
+        }
+    }
     return;
 }
 
