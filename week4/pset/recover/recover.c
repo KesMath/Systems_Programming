abd4463ac400b1b4aa5e2d50ab7f99b1299ed572
Kesler Mathieu <kesmathieu30@gmail.com>
2022-08-12 13:51:21 -0400

    submitting logic for cmd line parsing

--- week4/pset/recover/recover.c
+++ week4/pset/recover/recover.c
@@ -3,5 +3,16 @@
  
 int main(int argc, char *argv[])
 {
- 
+    if(argc != 2){
+        printf("Usage: ./recover IMAGE\n");
+        return 1;   
+    }
+
+    FILE *file = fopen(argv[1], "r");
+
+    if(file == NULL){
+        printf("Unable to open file. Exiting...\n");
+        return 1;
+    }
+
 }
