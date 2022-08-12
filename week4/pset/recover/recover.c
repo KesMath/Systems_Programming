6f563f93acc4cd40d5fc4e6a48c8dc3a0b856690
Kesler Mathieu <kesmathieu30@gmail.com>
2022-08-12 17:05:40 -0400

    submitting code now but need to work on logic for loop continuity

--- week4/pset/recover/recover.c
+++ week4/pset/recover/recover.c
@@ -12,27 +12,53 @@ int main(int argc, char *argv[])
         return 1;   
     }
 
-    FILE *file = fopen(argv[1], "r");
+    FILE *memory_card_file = fopen(argv[1], "r");
 
-    if(file == NULL){
+    if(memory_card_file == NULL){
         printf("Unable to open file. Exiting...\n");
         return 1;
     }
 
     // create an array buffer (optionally on the heap) 512 in length, each of 1 byte
-    // the multiplier is not needed but it helps to show that each index of this buffer is one byte/ 8 bits long
+    // the multiplier is not needed but it helps to show that each 'index' of this buffer is one byte/ 8 bits long
     BYTE *buffer = malloc(sizeof(BYTE) * BLOCK_SIZE);
+    char *filename = "000.jpg";
+    // need to capture and save first photo as 0th one!
+    int jpeg_cout = -1;
 
-    //read in 512 bytes at a time until memory card returns 0 bytes
-    while(fread(buffer, BLOCK_SIZE, 1, file) != 0){
-        
+    // read in 512 bytes at a time until memory card returns 0 bytes
+    while(fread(buffer, BLOCK_SIZE, 1, memory_card_file) != 0){
+        // check first 4 bytes to determine JPEG signature
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && 
+        (buffer[3] == 0xe0 || buffer[3] == 0xe1 || buffer[3] == 0xe2 || buffer[3] == 0xe3 ||
+         buffer[3] == 0xe4 || buffer[3] == 0xe5 || buffer[3] == 0xe6 || buffer[3] == 0xe7 ||
+         buffer[3] == 0xe8 || buffer[3] == 0xe9 || buffer[3] == 0xea || buffer[3] == 0xeb ||
+         buffer[3] == 0xec || buffer[3] == 0xed || buffer[3] == 0xee || buffer[3] == 0xef)){
+
+            // create new file
+            jpeg_cout++;
+            sprintf(filename, "%03i.jpg", jpeg_cout);
+            FILE *newFile = fopen(filename, "w");
+            fwrite(buffer, BLOCK_SIZE, 1, newFile);
+
+            // keep writing to that file until another jpeg signature is found!
+            // Recall that JPEGs are stored back to back in memory card
+            fread(buffer, BLOCK_SIZE, 1, memory_card_file);
+            while(buffer[0] != 0xff && buffer[1] != 0xd8 && buffer[2] != 0xff && 
+                !(buffer[3] == 0xe0 || buffer[3] == 0xe1 || buffer[3] == 0xe2 || buffer[3] == 0xe3 ||
+                  buffer[3] == 0xe4 || buffer[3] == 0xe5 || buffer[3] == 0xe6 || buffer[3] == 0xe7 ||
+                  buffer[3] == 0xe8 || buffer[3] == 0xe9 || buffer[3] == 0xea || buffer[3] == 0xeb ||
+                  buffer[3] == 0xec || buffer[3] == 0xed || buffer[3] == 0xee || buffer[3] == 0xef)){
+                fwrite(buffer, BLOCK_SIZE, 1, newFile);
+                //read in 512 bytes of memory into buffer again after writing previous buffer into new file
+                fread(buffer, BLOCK_SIZE, 1, memory_card_file);
+            }
+            // getting to this line means a new jpeg signature was found ... hence we close out file and jump back to outer loop conditional
+            fclose(newFile);
+        }
     }
     
-    // check JPEG byte signature of 0xff 0xd8 0xff 0xe0 -> 0xef
-
-    // if chunk or block is jpeg signature, write that to a new file and keep on writing those bytes
-    // until another 512 byte block containing 4 byte JPEG signature appears... then you will close out that file
-    // and write to a new jpeg file until another signature is detected
+    fclose(memory_card_file);
     free(buffer);
 
 }
