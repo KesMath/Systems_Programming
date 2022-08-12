cfb722f40f955ebf31ea16934a749acfcfd5e3d1
Kesler Mathieu <kesmathieu30@gmail.com>
2022-08-12 13:56:20 -0400

    adding commentary

--- week4/pset/recover/recover.c
+++ week4/pset/recover/recover.c
@@ -15,4 +15,12 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    //read in 512 bytes at a time until memory card returns 0 bytes
+    
+    // check JPEG byte signature of 0xff 0xd8 0xff 0xe0 -> 0xef
+
+    // if chunk or block is jpeg signature, write that to a new file and keep on writing those bytes
+    // until another 512 byte block containing 4 byte JPEG signature appears... then you will close out that file
+    // and write to a new jpeg file until another signature is detected
+
 }
